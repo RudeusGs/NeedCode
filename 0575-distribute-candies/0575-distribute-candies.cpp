dd49647f07469bb73398a0714e1ca40355ced9f2
Rudeus Grey <129082226+RudeusGs@users.noreply.github.com>
2025-09-15 22:09:46 +0700

    Time: 123 ms (33.84%), Space: 120.1 MB (56.06%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_set<int> s(candyType.begin(), candyType.end());
+        
+         return min(s.size(), candyType.size() /2);
+    }
+};
