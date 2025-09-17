239b5749e415a93432eea2b5d0776332ec04fb92
Rudeus Grey <129082226+RudeusGs@users.noreply.github.com>
2025-09-17 09:53:05 +0700

    Time: 6 ms (10.28%), Space: 9.3 MB (80.53%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.size() != t.size()) return false;
-        map<char, int> mp1;
-        map<char, int> mp2;
-        bool check = true;
+        if (s.size() != t.size()) return false;
+        map<char, char> mp1;
+        map<char, char> mp2;
         for(int i = 0; i < s.size(); i++){
-            char a = s[i];
-            char b = t[i];
-            if(mp1.count(a)){
-                if(mp1[a] != b) return false;
-            }
-            else{
-                mp1[a] = b;
-            }
-            if(mp2.count(b)){
-                if(mp2[b] != a) return false;
-            }
-            else{
-                mp2[b] = a;
-            }
+            char cs = s[i], ct = t[i];
+            if(mp1.count(cs) && mp1[cs] != ct) return false;
+            if(mp2.count(ct) && mp2[ct] != cs) return false;
+            mp1[cs] = ct;
+            mp2[ct] = cs;
         }
-        return check;
+        return true;
     }
 };
