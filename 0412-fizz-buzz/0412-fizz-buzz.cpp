58f25a47e3a63b4c4246fc41d191ba202a175c90
Rudeus Grey <129082226+RudeusGs@users.noreply.github.com>
2025-09-17 10:56:57 +0700

    Time: 3 ms (29.5%), Space: 11 MB (93.19%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string> res;
+        res.reserve(n);
         for(int i = 1; i <= n; i++){
             if(i % 3 == 0 && i % 5 == 0) res.push_back("FizzBuzz");
             else if(i % 3 == 0) res.push_back("Fizz");
