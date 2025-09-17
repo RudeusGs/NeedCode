e99c614b32afcf2fb34beeee6cc0d1ff1449ff55
Rudeus Grey <129082226+RudeusGs@users.noreply.github.com>
2025-09-17 10:50:26 +0700

    Time: 4 ms (25.3%), Space: 10.4 MB (46.97%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
+    bool isVowel(char k){
+        return k == 'a' || k == 'e'|| k == 'i' || k == 'o' || k == 'u' 
+        || k == 'A' || k == 'E' || k == 'I' || k == 'O' || k == 'U';
+    }
     string reverseVowels(string s) {
-        vector<char> v;
-        vector<int> vindex;
-        int n = s.size();
-        for(int i = 0; i < n; i++){
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
-            s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
-                v.push_back(s[i]);
-                vindex.push_back(i);
-            }
-        }
-        reverse(v.begin(), v.end());
-        for(int i = 0; i < vindex.size(); i++){
-            s[vindex[i]] = v[i];
+        int l = 0, r = s.size() - 1;
+        while(l <= r){
+            if(!isVowel(s[l])) { l++; continue; }
+            if(!isVowel(s[r])) { r--; continue; }
+            swap(s[l], s[r]);
+            l++;
+            r--;
         }
         return s;
+        // vector<char> v;
+        // vector<int> vindex;
+        // int n = s.size();
+        // for(int i = 0; i < n; i++){
+        //     if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+        //     s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+        //         v.push_back(s[i]);
+        //         vindex.push_back(i);
+        //     }
+        // }
+        // reverse(v.begin(), v.end());
+        // for(int i = 0; i < vindex.size(); i++){
+        //     s[vindex[i]] = v[i];
+        // }
+        // return s;
     }
 };
