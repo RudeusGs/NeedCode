6ad702c1aeaf8fbad33783d3496f043a110a7177
Rudeus Grey <129082226+RudeusGs@users.noreply.github.com>
2025-09-17 10:51:09 +0700

    Time: 4 ms (25.3%), Space: 11.4 MB (8.91%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -5,29 +5,28 @@ public:
         || k == 'A' || k == 'E' || k == 'I' || k == 'O' || k == 'U';
     }
     string reverseVowels(string s) {
-        int l = 0, r = s.size() - 1;
-        while(l <= r){
-            if(!isVowel(s[l])) { l++; continue; }
-            if(!isVowel(s[r])) { r--; continue; }
-            swap(s[l], s[r]);
-            l++;
-            r--;
+        // int l = 0, r = s.size() - 1;
+        // while(l <= r){
+        //     if(!isVowel(s[l])) { l++; continue; }
+        //     if(!isVowel(s[r])) { r--; continue; }
+        //     swap(s[l], s[r]);
+        //     l++;
+        //     r--;
+        // }
+        vector<char> v;
+        vector<int> vindex;
+        int n = s.size();
+        for(int i = 0; i < n; i++){
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+            s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+                v.push_back(s[i]);
+                vindex.push_back(i);
+            }
+        }
+        reverse(v.begin(), v.end());
+        for(int i = 0; i < vindex.size(); i++){
+            s[vindex[i]] = v[i];
         }
         return s;
-        // vector<char> v;
-        // vector<int> vindex;
-        // int n = s.size();
-        // for(int i = 0; i < n; i++){
-        //     if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
-        //     s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
-        //         v.push_back(s[i]);
-        //         vindex.push_back(i);
-        //     }
-        // }
-        // reverse(v.begin(), v.end());
-        // for(int i = 0; i < vindex.size(); i++){
-        //     s[vindex[i]] = v[i];
-        // }
-        // return s;
     }
 };
