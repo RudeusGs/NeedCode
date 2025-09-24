6ad1d2db41995952819165940613bb03b5c5d337
Rudeus Grey <129082226+RudeusGs@users.noreply.github.com>
2025-09-24 18:52:30 +0700

    Time: 5 ms (24.76%), Space: 8.8 MB (85.48%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        map<char, pair<int,int>> mp;
         int res = 1;
         int n = word.size();
-        for (int i = 0; i < n;) {
+        for(int i = 0; i < n;){
             char c = word[i];
             int count = 0;
-            while (i < n && word[i] == c) {
+            while(i < n && word[i] == c){
                 count++;
                 i++;
             }
-            mp[c] = {i - 1, count};
             res += count - 1;
         }
         return res;
