65b088c956746feeeae4beef42ee5e4980112396
Rudeus Grey <129082226+RudeusGs@users.noreply.github.com>
2025-10-01 10:04:26 +0700

    Time: 3 ms (6.66%), Space: 9.2 MB (56.97%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return {};
+        map<char, vector<char>> mp;
+        mp['2'] = {'a','b','c'};
+        mp['3'] = {'d','e','f'};
+        mp['4'] = {'g','h','i'};
+        mp['5'] = {'j','k','l'};
+        mp['6'] = {'m','n','o'};
+        mp['7'] = {'p','q','r','s'};
+        mp['8'] = {'t','u','v'};
+        mp['9'] = {'w','x','y','z'};
+        vector<string> res = {""};
+        for(char d : digits){
+            vector<string> temp;
+            for(string &s : res){
+                for(char c : mp[d]){
+                    temp.push_back(s + c);
+                }
+            }
+            res.swap(temp);
+        }
+        return res;
+    }
+};
