d9a4c769b402d14f38a2c72176955bd15f089353
Rudeus Grey <129082226+RudeusGs@users.noreply.github.com>
2025-10-10 13:46:16 +0700

    Time: 0 ms (100%), Space: 7.8 MB (30.16%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.cpp
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        vector<bool> dp(n + 1, false);
-        dp[1] = false;
-        for(int i = 2; i <= n; i++){
-            for(int x = 1; x < i; x++){
-                if(i % x == 0 && dp[i - x] == false){
-                    dp[i] = true;
-                    break;
-                }
-            }
-        }
-        return dp[n];
+        return !(n & 1);
     }
 };
